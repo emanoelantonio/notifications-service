70767b1a6b597c76abb0da1ee5af4de7fc58c151
Emanoel Antonio <dev.emanoel@protonmail.com>
2022-12-17 16:27:32 -0300

    Controllers and Data Tranfer Object

---
+++ src/infra/http/DTOs/create-notification-body.ts
@@ -0,0 +1,15 @@
+// DATA TRANSFER OBJECT - DTO
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
+  content: string;
+
+  @IsNotEmpty()
+  category: string;
+}
---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from 'src/app/use-cases/send-notification';
+import { CreateNotificationBody } from '../DTOs/create-notification-body';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+    return {
+      notification: {
+        id: notification.id,
+        content: notification.content.value,
+        category: notification.category,
+        recipientId: notification.recipientId,
+      },
+    };
+  }
+}
